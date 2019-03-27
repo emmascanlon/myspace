410fffd9390c9eb80ec4c26c3b49f9401215b7db
Emma Scanlon <emmascanlon4@gmail.com>
2019-03-27 14:06:55 -0600

    initial commit

---
+++ client/src/providers/AuthProvider.js
@@ -0,0 +1,34 @@
+import React from "react";
+import axios from "axios";
+
+const AuthContext = React.createContext() ;
+
+export const AuthConsumer = AuthContext.Consumer;
+
+export class AuthProvider extends React.Component {
+  state = { user: null, };
+
+  handleRegister = () => {
+  }
+
+  handleLogin = () => {
+  }
+
+  handleLogout = () => {
+  }
+
+  render() {
+    return (
+      <AuthContext.Provider value={{
+        ...this.state,
+        authenticated: this.state.user !== null,
+        handleRegister: this.handleRegister,
+        handleLogin: this.handleLogin,
+        handleLogout: this.handleLogout,
+        setUser: (user) => this.setState( {user: }),
+      }}>
+{this.props.children}
+</AuthContext.Provider>
+    )
+}
+}
